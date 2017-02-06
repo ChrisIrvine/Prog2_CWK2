29b7bbcae67eaf54f02646938eeed0b3b3535175
Christopher Irvine <christopherirvine@Christophers-iMac.local>
2017-02-06 13:38:30 +0000

    Adding Q1 files

---
+++ src/Q1/Card.java
@@ -0,0 +1,292 @@
+/*
+ * Author:      ruw12gbu, 100036248
+ * 
+ * Description: This class is the backbone of the entire Cheat! project. It
+ *              contains two enums, Rank and Suit, that for each Card Object. 
+ *              There are 13 Ranks and 4 Suits, creating a possible 52 
+ *              combinations of Card Objects. Each Rank and Suit has a value
+ *              assigned to it that will be used whilst playing Cheat! This 
+ *              class also contains a few Override methods that override the 
+ *              default methods in the Comparable class. This allows proper 
+ *              comparisons between two Card Objects (in order to see if which
+ *              one is higher, lower or if they are equal). This class is also
+ *              Serializable (serializationID 100).
+ *
+ * Version:     1.0 - Created
+ *              1.1 - Override methods reworked
+ *              1.2 - Override methods refined
+ *              1.3 - Adjustments from test results.
+ *              1.4 - Comments refined.
+ */
+package Q1;
+import java.io.Serializable;
+import java.util.Comparator;
+
+/**
+ * @author ruw12gbu, 100036248
+ */
+public class Card implements Comparable<Card>, Serializable
+{
+    //Declare serialisationID as 100
+    static final long serialVersionUID = 100;
+
+    //Create enum called Rank
+    public enum Rank
+    {
+        //Declare enum values for the ranks
+        TWO (2),
+        THREE (3),
+        FOUR (4),
+        FIVE (5),
+        SIX (6),
+        SEVEN (7),
+        EIGHT (8),
+        NINE (9),
+        TEN (10),
+        JACK (10),
+        QUEEN (10),
+        KING (10),
+        ACE (11);
+        
+        //Declare private value field
+        private int value;
+        
+        /**
+         * Constructor method for the Rank enum. Takes a single integer 
+         * parameter.
+         * @param r - the value of the rank 
+         */
+        Rank (int r)
+        {
+            value = r;
+        }
+        
+        /**
+         * Method to return the next value in the enum list from a given card. 
+         * Uses the Card object that was used to call this method. If the Card 
+         * object has a rank value of a FOUR then FIVE will be returned.
+         * @return - the enum value that is next on the enum list.
+         */
+        public Rank getNext()
+        {
+            //Create rank object to hold the value of the next rank.
+            Rank next;
+            //if condition to check position is not at the end of the enum list.
+            if(this.ordinal() < Rank.values().length - 1)
+            {
+                /*
+                 * if position is not at the end of the list then select the
+                 * next position on the list.
+                 */
+                next = Rank.values()[this.ordinal() + 1];
+            }
+            else
+            {
+                // Otherwise go to the front of the list.
+                next = Rank.values()[0];
+            }
+            //Return next object.
+            return next;
+        }
+        
+        /**
+         * Accessor Method to return the Rank Value of the current Card Object. 
+         * @return
+         */
+        public int getValue()
+        {
+            return this.value;
+        }
+    }
+    
+    //Create enum called Suit
+    public enum Suit
+    {
+        //Declare enum values for Suit
+        CLUBS (1),
+        HEARTS (2),
+        DIAMONDS (3),
+        SPADES (4);
+
+        //Create int value to hold the Suit Value
+        private final int suitNum;
+
+        /**
+         * Constructor method for the Suit enum. Takes a single integer
+         * parameter that sets the Suit of the card.
+         * @param s 
+         */
+        Suit(int s)
+        {
+            this.suitNum = s;
+        }
+
+        /**
+         * Accessor method to return the Suit Number of the current card object.
+         * @return 
+         */
+        public int getSuitNum()
+        {
+            return this.suitNum;
+        }
+    }
+    
+    //Create Rank variable
+    Rank rank;
+    //Create Suit variable
+    Suit suit;
+
+    public int getRankValue()
+    {
+        return this.rank.value;
+    }
+    /**
+     * Constructor for Card Objects. This method takes two parameters (detailed)
+     * below. Used to fill a Deck of Cards (52) card objects.
+     * @param r - rank of the card, between TWO & ACE (ACE High)
+     * @param s - suit of the card, HEARTS, CLUBS, DIAMONDS, SPADES
+     */
+    Card(Rank r, Suit s)
+    {
+        this.rank = r;
+        this.suit = s;
+    }
+
+    /**
+     * Accessor Method to return this Card's Rank enum.
+     * @return 
+     */
+    public Rank getRank()
+    {
+        return rank;
+    }
+
+    /**
+     * Accessor Method to return this Card's Suit enum.
+     * @return 
+     */
+    public Suit getSuit()
+    {
+        return suit;
+    }
+
+    /**
+     * Method to override the Comparator class' compareTo method. This compareTo 
+     * method will check to see which card is lower. If the first card is lower
+     * a negative number will be the result, otherwise it will be a positive 
+     * number. that the rank of the cards are the same. If the ranks are the 
+     * same, the suits are compared, results are interpetted in the same manner.
+     * @param c - Card to be compared against.
+     * @return - Negative = First Card is lower. Positive = Second Card is lower
+     */
+    @Override
+    public int compareTo(Card c)
+    {
+        //Check for identical ranks
+        if(this.rank == c.getRank())
+        {
+            //Current Card Suit value - Compare Card Suit value
+            return this.suit.ordinal() - c.getSuit().ordinal();
+        }
+        else
+        {
+            //Current Card Rank value - Compare Card Rank value
+            return this.rank.ordinal() - c.getRank().ordinal();
+        }
+    }
+    
+    /**
+     * Method to override the String class' toString method. Simply outputs the
+     * current Card object's information in a readable manner.
+     * @return 
+     */
+    @Override
+    public String toString()
+    {
+        StringBuilder str = new StringBuilder();
+
+        str.append(rank).append(" ").append(suit);
+
+        return str.toString();
+    }
+    
+    /**
+     * Method to return the difference in the ranks between the two cards.
+     * @param c - Card to be compared against.
+     * @return 
+     */
+    public int difference(Card c)
+    {
+        //Current Card Rank position - Compare Card Rank position
+        return this.rank.ordinal() - c.getRank().ordinal();
+    }
+    
+    /**
+     * Method to return the difference in value in the ranks between two cards.
+     * @param c - Card to be compared against.
+     * @return 
+     */
+    public int differenceValue(Card c)
+    {
+        //Current Card Rank value - Compare Card Rank value
+        return this.rank.getValue() - c.getRank().getValue();
+    }
+
+    /*
+     * Nested class to be used to sort the cards into a descending order. Has
+     * only one method that is to call the compareTo(Card c) method (as noted
+     * above). Descending order is to be done by order of Rank, Suit is to be
+     * only used when two Card Objects have the same Rank.
+     */
+    public static class CompareDescending implements Comparator<Card>
+    {
+        /**
+         * Method to override the compare() method of the Comparator class. This
+         * calls the compareTo(Card c) method as listed above. Returns a single 
+         * integer. Negative means Card c is lower. Positive means Card c1 is 
+         * lower.
+         * @param c - Current Card Object.
+         * @param c1 - Card Object to compare against.
+         * @return 
+         */
+        @Override
+        public int compare (Card c, Card c1)
+        {
+            //Call and return the compareTo method
+            return c1.compareTo(c);
+        }
+    }
+    
+    /*
+     * Nested class to be used to sort Card Objects into ascending order of
+     * suit. Once sorted into suits cards should be in ascending order by rank.
+     * Has one method, to override the compare() method.
+     */
+    public static class CompareSuit implements Comparator<Card>
+    {
+        /**
+         * Method to override the compare() method of the Comparator Class. This
+         * compares the position on the enum. Checks to see if the Suits are the
+         * same. If they are the Ranks are checked, otherwise Suits are checked.
+         * @param c - First Card Object
+         * @param c1 - Second Card Object
+         * @return - Negative Integer = first Card Object is higher
+         *           Positive Integer = second Card Object is higher
+         */
+        @Override
+        public int compare(Card c, Card c1)
+        {
+            //Check if Suits are the the same
+            if (c.getSuit() == c1.getSuit())
+            {
+                //if Suits are the same then test the Ranks
+                return c.getRank().ordinal() - c1.getRank().ordinal();
+            }
+            else
+            {
+                //Otherwise, check test the Suits
+                return c.getSuit().ordinal() - c1.getSuit().ordinal();
+            }
+        }
+    }
+}
---
+++ src/Q1/CardTest.java
@@ -0,0 +1,339 @@
+///*
+// * Main Class to test the Hand.java file. Each method is tested in any/all
+// * cases. 
+// */
+//package Q1;
+//
+//import Q1.Card.Rank;
+//import Q1.Card.Suit;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+///**
+// * @author ruw12gbu, 100036248
+// */
+//public class CardTest 
+//{
+//    public static void main(String[] args) throws FileNotFoundException,
+//            IOException, ClassNotFoundException
+//    {
+//        //int object to be used whilst testing
+//        int i;
+//        //Card Objects to be used whilst testing
+//        Card card = new Card(Rank.ACE, Suit.SPADES);
+//        Card cardTwo = new Card(Rank.EIGHT, Suit.DIAMONDS);
+//        Card cardThree = new Card(Rank.THREE, Suit.CLUBS);
+//        //Create, shuffle and print out a Deck Object
+//        Deck deck = new Deck();
+//        deck.shuffle();
+//        System.out.println("Shuffled Deck: ");
+//        //Iterate over the deck of cards and print out each card
+//        for (i = 0; i < deck.size(); i++) 
+//        {
+//            System.out.println(deck.getCurrent(i).toString());
+//        }
+//        
+//        //Formatting
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("Creating an Array of Card Objects...\n");
+//        
+//        //Create and fill an array of Card Objects, from the Deck Object
+//        Card[] cardArray = new Card[5];
+//        cardArray[0] = deck.deal();
+//        cardArray[1] = deck.deal();
+//        cardArray[2] = deck.deal();
+//        cardArray[3] = deck.deal();
+//        cardArray[4] = deck.deal();
+//        //Print out cardArray
+//        System.out.println("Cards in cardArray: \n");
+//        for (i = 0; i < cardArray.length; i++) 
+//        {
+//            System.out.println(cardArray[i].toString());            
+//        }
+//
+//        //Formatting
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("Testing the constructor methods... \n");
+//        //Create a new, empty Hand Object.
+//        Hand handOne = new Hand();
+//        //Prove hand is empty by printing out the number of cards in the hand.
+//        System.out.println("Hand One has " + handOne.handSize() + " cards.");
+//        //Create a new Hand Object that is filled by an Array of Card Objects.
+//        Hand handTwo = new Hand(cardArray);
+//        //Test if filledHand has 5 Card Objects from cardArray
+//        System.out.println("\nThe Cards in Hand Two are: \n" 
+//                + handTwo.toString());
+//        //Create a new Hand Object that copies the card from filledHand
+//        Hand handThree = new Hand(handTwo);
+//        //Test if cards are copies correctly from filledHand
+//        System.out.println("\nThe Cards in Hand Three are: \n" 
+//                + handThree.toString());
+//        
+//        //Formatting
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("Testing the add methods...\n");
+//        
+//        /* Test the first add method, adds a single card at a time by dealing 
+//         * 5 Card Objects from the Deck Object. Also test the getCards() method
+//         * to return the cards for printing.
+//         */
+//        //fill handOne
+//        for (i = 0; i < 5; i++) 
+//        {
+//            handOne.add(deck.deal());
+//        }
+//        //print handOne
+//        System.out.println("Cards in Hand One: " + handOne.getCards());
+//        
+//        /* Test the second add method, adds a Collection of Cards to a Hand 
+//         * Object. Begin testing by wiping handTwo, then creating a Collection
+//         * finally adding the Collection to handTwo before printing out handTwo.
+//         */
+//        //Clear handTwo
+//        handTwo.clearHand();
+//        //Create a List Collection of Card Objects
+//        List l = new ArrayList();
+//        //Add 5 cards to the List Collection, cardList
+//        for (i = 0; i < 5; i++) 
+//        {
+//            l.add(deck.deal());
+//        }
+//        //Add the Card Objects in cardList to handTwo
+//        handTwo.add(l);
+//        //Print handTwo
+//        System.out.println("Cards in Hand Two: " + handTwo.getCards());
+//        
+//        /* Test the third add method, that takes a Hand object and adds the 
+//         * cards from that hand. Clearing the previous hand. Test starts by 
+//         * clearing handThree. 
+//         */
+//        //Clear handThree
+//        handThree.clearHand();
+//        //Add the cards from handOne to handThree
+//        handThree.add(handOne);
+//        //Print handThree
+//        System.out.println("Cards in Hand Three: " + handThree.getCards() 
+//                + "\n");
+//        
+//        //Formatting
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("Testing the remove methods...\n");
+//        
+//        //Test the first remove method, removing a single card.
+//        //Add card to be removed
+//        handOne.add(card);
+//        //Print out handOne
+//        System.out.println("Cards in handOne: " + handOne.getCards());
+//        //remove(Card c) method returns a boolean value so need if condition to 
+//        //check
+//        if(handOne.remove(card) == true)
+//        {
+//            System.out.println(card + " successfully removed from handOne.");
+//        }
+//        else
+//        {
+//            System.out.println(card + " could not be found in handOne");
+//        }
+//        //Print out hand after removed card
+//        System.out.println("Cards in handOne: " + handOne.getCards() + "\n");
+//        
+//        //Test the second remove method that all cards from another Hand Object.
+//        //Clear a hand to be used in testing
+//        handTwo.clearHand();
+//        //Fill cleared hand with some test cards
+//        handTwo.add(card);
+//        handTwo.add(cardTwo);
+//        handTwo.add(cardThree);
+//        //Add the same cards to handOne
+//        handOne.add(card);
+//        handOne.add(cardTwo);
+//        handOne.add(cardThree);
+//        //Print out handOne and handTwo
+//        System.out.println("Cards in handOne: " + handOne.getCards());
+//        System.out.println("Cards in handTwo: " + handTwo.getCards());
+//        //Remove the cards in handTwo from handOne
+//        if(handOne.remove(handTwo) == true)
+//        {
+//            System.out.println(handTwo.getCards() + " sucessfully removed from "
+//                    + "handOne");
+//        }
+//        else
+//        {
+//            System.out.println(handTwo.getCards() + " could not be found in "
+//                    + "handOne");
+//        }
+//        //Print out handOne
+//        System.out.println("Cards in handOne: " + handOne.getCards());
+//        
+//        /* Test the third remove method that removes and returns a card from a 
+//         * Hand Object.
+//         */
+//        //Print out handThree
+//        System.out.println("\nCards in handThree: " + handThree.getCards());
+//        //Remove the 4th card (index starts at 0)
+//        handThree.remove(3);
+//        //Print out handThree
+//        System.out.println("Cards in handThree: " + handThree.getCards());
+//        
+//        //Formatting
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("Testing the sort methods...\n");
+//        
+//        //Test the sortAscending method
+//        handOne.sortAscending();
+//        System.out.println("handOne sorted into Ascending Order: "
+//                + handOne.getCards());
+//        
+//        //Test the sortDescending method
+//        handOne.sortDescending();
+//        System.out.println("handOne sorted into Descending Order: "
+//                + handOne.getCards());
+//        
+//        //Formatting 
+//        System.out.println("\nCheck that Suit Order is preserved...");
+//        
+//        //Test to check suit order is preserved
+//        //Clear handTwo
+//        handTwo.clearHand();
+//        //Replace cardArray elements with custom made cards
+//        cardArray[0] = new Card(Rank.ACE, Suit.SPADES);
+//        cardArray[1] = new Card(Rank.ACE, Suit.DIAMONDS);
+//        cardArray[2] = new Card(Rank.ACE, Suit.HEARTS);
+//        cardArray[3] = new Card(Rank.ACE, Suit.CLUBS);
+//        cardArray[4] = new Card(Rank.FOUR, Suit.HEARTS);
+//        //fill handTwo from cardArray
+//        for(i = 0; i < cardArray.length; i++)
+//        {
+//            handTwo.add(cardArray[i]);
+//        }
+//        //Print handTwo
+//        System.out.println("\nCards in handTwo: " + handTwo.getCards());
+//        //Sort handTwo into Ascending Order
+//        handTwo.sortAscending();
+//        //Print sorted handTwo
+//        System.out.println("handTwo sorted into Ascending Order: " 
+//                + handTwo.getCards());
+//        //Sort handTwo into Descending Order
+//        handTwo.sortDescending();
+//        //Print sorted handTwo
+//        System.out.println("handTwo sorted into Descending Order: " 
+//                + handTwo.getCards());
+//        
+//        //Formatting
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("Testing the countSuit & countRank methods...\n");
+//        
+//        //Create a new Deck, for the purposes of testing
+//        deck.newDeck();
+//        deck.shuffle();
+//        //Clear and fill handOne with 10 new cards from the refreshed deck
+//        handOne.clearHand();
+//        for(i = 0; i < 10; i++)
+//        {
+//            handOne.add(deck.deal());
+//        }
+//        //Print handOne
+//        System.out.println("Cards in handOne: " + handOne.getCards());
+//        //Count the cards of each of the suits
+//        System.out.println("Number of...");
+//        System.out.println("CLUBS : " + handOne.countSuit(Suit.CLUBS));
+//        System.out.println("DIAMONDS : " + handOne.countSuit(Suit.DIAMONDS));
+//        System.out.println("HEARTS : " + handOne.countSuit(Suit.HEARTS));
+//        System.out.println("SPADES : " + handOne.countSuit(Suit.SPADES));
+//        System.out.println("... in handOne\n");
+//        
+//        System.out.println("Number of...");
+//        System.out.println("TWOs : " + handOne.countRank(Rank.TWO));
+//        System.out.println("THREEs : " + handOne.countRank(Rank.THREE));
+//        System.out.println("FOURs : " + handOne.countRank(Rank.FOUR));
+//        System.out.println("FIVEs : " + handOne.countRank(Rank.FIVE));
+//        System.out.println("SIXs : " + handOne.countRank(Rank.SIX));
+//        System.out.println("SEVENs : " + handOne.countRank(Rank.SEVEN));
+//        System.out.println("EIGHTs : " + handOne.countRank(Rank.EIGHT));
+//        System.out.println("NINEs : " + handOne.countRank(Rank.NINE));
+//        System.out.println("TENs : " + handOne.countRank(Rank.TEN));
+//        System.out.println("JACKs : " + handOne.countRank(Rank.JACK));
+//        System.out.println("QUEENs : " + handOne.countRank(Rank.QUEEN));
+//        System.out.println("KINGs : " + handOne.countRank(Rank.KING));
+//        System.out.println("ACEs : " + handOne.countRank(Rank.ACE));
+//        System.out.println(".. in handOne");
+//        
+//        //Formatting
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("Testing the handValue method...\n");
+//        
+//        System.out.println("The total value of the ranks in handOne is: "
+//                + handOne.handValue());
+//        
+//        //Formatting
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("Testing the toString methods...\n");
+//        
+//        System.out.println("Cards in handOne: \n" + handOne.toString());
+//        
+//        //Formatting
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("Testing the isFlush & isStraight methods...\n");
+//        
+//        //Testing isFlush()
+//        
+//        //Clear handTwo, to install a Flush Hand
+//        handTwo.clearHand();
+//        //Replace cardArray elements with custom made cards
+//        cardArray[0] = new Card(Rank.TWO, Suit.DIAMONDS);
+//        cardArray[1] = new Card(Rank.THREE, Suit.DIAMONDS);
+//        cardArray[2] = new Card(Rank.FOUR, Suit.DIAMONDS);
+//        cardArray[3] = new Card(Rank.FIVE, Suit.DIAMONDS);
+//        cardArray[4] = new Card(Rank.SIX, Suit.DIAMONDS);
+//        //fill handTwo from cardArray
+//        for(i = 0; i < cardArray.length; i++)
+//        {
+//            handTwo.add(cardArray[i]);
+//        }
+//        //Print handTwo
+//        System.out.println("\nCards in handTwo: " + handTwo.getCards());
+//        //Check if handTwo contains a Flush, expected result is it should do.
+//        if(handTwo.isFlush() == true)
+//        {
+//            System.out.println("handTwo contains a Flush.");
+//        }
+//        else
+//        {
+//            System.out.println("handTwo does not contain a Flush.");
+//        }
+//        //Check if handOne contains a Flush, it should not (althought it could 
+//        //as cards are random.
+//        if(handOne.isFlush() == true)
+//        {
+//            System.out.println("handOne contains a Flush.");
+//        }
+//        else
+//        {
+//            System.out.println("handOne does not contain a Flush.");
+//        }
+//        
+//        //Test the isStraight() method
+//        
+//        //Check if handTwo contains a straight, it has been planted with one.
+//        if(handTwo.isStraight() == true)
+//        {
+//            System.out.println("handTwo contains a Straight.");
+//        }
+//        else
+//        {
+//            System.out.println("handTwo does not contain a Straight.");
+//        }
+//        //Check if handOne contains a straight, it is highly unlikely (but 
+//        //possible).
+//        if(handOne.isStraight() == true)
+//        {
+//            System.out.println("handOne contains a Straight.");
+//        }
+//        else
+//        {
+//            System.out.println("handOne does not contain a Straight.");
+//        }
+//    }
+//}
