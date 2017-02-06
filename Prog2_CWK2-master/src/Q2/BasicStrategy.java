9917f0934bb71315e721380346190a0ae8065bb9
Christopher Irvine <christopher@irvinegroup.eu>
2017-02-06 17:34:56 +0000

    Add files via upload

---
+++ Prog2_CWK2-master/src/Q2/BasicStrategy.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q2;
+
+import Q1.Hand;
+import Q1.Card;
+import java.util.Random;
+//import java.util.Iterator;
+
+/**
+ *
+ * @author ruw12gbu, 100036248
+ */
+public class BasicStrategy implements Strategy {
+
+    @Override
+    public boolean cheat(Bid bidCard, Hand playerHand) {
+        int count = playerHand.countRank(bidCard.getRank());
+
+        if (count == 0) {
+            return true;
+        }
+
+        count = playerHand.countRank(bidCard.getRank().getNext());
+        if (count == 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public Bid chooseBid(Bid b, Hand h, boolean cheat) {
+        Random random = new Random();
+        Bid bid;
+
+        if (cheat) {
+            Hand hand = new Hand();
+
+            Card card = h.remove(random.nextInt(h.handSize()));
+            hand.add(card);
+            bid = new Bid(hand, b.getRank().getNext());
+        } else {
+            Hand hand = new Hand();
+            Card.Rank save = Card.Rank.TWO;        //next one
+            for (Card.Rank rank : Card.Rank.values()) {
+                boolean take = false;
+
+                if (b.getRank().getValue() == 13
+                        || (b.getRank().ordinal() >= rank.ordinal()
+                        || h.countRank(rank) <= 0)) {
+                    if (h.countRank(rank) > 0
+                            && b.getRank().getValue() == 13) {
+
+                        if (rank.getValue() == 2 || rank.getValue() == 13) {
+                            take = true;
+                        }
+                    } else {
+                        take = true;
+                    }
+                }
+                if (!take) {
+                } else {
+                    save = rank;
+                    for (int i = 0; i < h.handSize(); i++) {
+                        Card temporary = h.getCard(i);
+                        if (temporary.getRank() != rank) {
+                        } else {
+                            hand.add(temporary);
+                        }
+                    }
+                    break;
+                }
+            }
+            bid = new Bid(hand, save);
+            h.remove(hand);
+        }
+        return bid;
+    }
+
+    @Override
+    public boolean callCheat(Hand playerHand, Bid bidCard) {
+        int player = playerHand.countRank(bidCard.getRank());
+        int bid = bidCard.getCount();
+
+        bid -= player;
+        return bid < 0;
+    }
+}
---
+++ Prog2_CWK2-master/src/Q2/Bid.java
@@ -0,0 +1,49 @@
+package Q2;
+
+import Q1.*;
+//import solution_part2.*;
+
+public class Bid 
+{
+    Hand h;
+    Card.Rank r;
+    
+    public Bid()
+    {
+        h=new Hand();
+        r=Card.Rank.TWO; 
+    }
+    public Bid(Hand hand,Card.Rank bidRank)
+    {
+        h=hand;
+        r=bidRank;
+    }
+    
+    public void setHand(Hand hand)
+    { 
+       h=hand;
+    }
+    
+    public void setRank(Card.Rank rank)
+    { 
+       r=rank;
+    }
+
+    public Hand getHand()
+    { 
+       return h;
+    }
+    
+    public int getCount()
+    { 
+       return h.handSize();
+    }
+    public Card.Rank getRank()
+    { 
+       return r;
+    }
+    public String toString()
+    {
+        return h.handSize()+" x "+r;
+    }	
+}	
